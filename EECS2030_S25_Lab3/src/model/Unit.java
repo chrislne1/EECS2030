7b886749bb553b8f10d580b1d6a10b3aed978702
Christoir <kaijames437@gmail.com>
2025-10-27 18:07:23 -0400

    lab2 nd part of 3

---
+++ EECS2030_S25_Lab3/src/model/Unit.java
@@ -0,0 +1,55 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+public class Unit {
+	private String name;
+	private double length, width;
+	private boolean feet = true;
+	
+	public Unit(String name, int width, int length) {
+		this.name = name;
+		this.width = width;
+		this.length = length;
+	}
+	
+	public String toString() {
+		String str ="";
+		
+		//measured in feet
+		//"A unit of 126 square feet (14' wide and 9' long) functioning as Master Room"
+		if(feet) {
+			int area = (int) (length * width);
+			str = "A unit of " + area +" square feet (" + (int)width + "' wide and " + (int)length + "' long) functioning as " + name;
+		}
+		
+		//measured in ms
+		//"A unit of 11.71 square meters (4.27 m wide and 2.74 m long) functioning as Master Room"
+		else {
+			double templength =  Math.round(length * 100.0) / 100.0;
+			double tempwidth  = Math.round(width * 100.0) / 100.0;
+			double area = Math.round(length * width * 100.0) / 100.0;
+			
+			str = "A unit of " + area + " square meters (" + tempwidth + " m wide and " + templength + " m long) functioning as "+ name;
+		}
+		return str;
+	}
+	
+	public void toggleMeasurement() {
+		//* Use this conversion formula: One foot is equal to 0.3048 meter.
+		// * Each value of meters and square meters should be displayed with 2 digits after the decimal point.
+		
+		//feet to meters
+		if (feet) {
+			feet = false;
+			length = length * 0.3048;
+			width  = width  * 0.3048;
+		}
+		//meters to feet
+		else {
+			feet = true;
+			length = (int) Math.round(length /.3048);
+			width = (int) Math.round(width /.3048);
+		}
+	}
+}
